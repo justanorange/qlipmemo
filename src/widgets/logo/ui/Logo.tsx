ddf5e9e577edc1b8bb15f96d70b708b87f291170
Vladimir Lobas <lobasvv@icloud.com>
2025-10-26 23:54:21 +0600

    Quick logo image path fix

--- src/widgets/logo/ui/Logo.tsx
+++ src/widgets/logo/ui/Logo.tsx
@@ -4,6 +4,6 @@ export const Logo = () => {
   const isLogoSpinning = useUIStore((state) => state.isLogoSpinning);
   
   return (
-    <img className={`size-10 ${isLogoSpinning ? 'logo--spinning' : ''}`} src="/logo.svg" alt="Logo" />
+    <img className={`size-10 ${isLogoSpinning ? 'logo--spinning' : ''}`} src="./logo.svg" alt="Logo" />
   )
 }
